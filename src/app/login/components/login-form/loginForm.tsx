4c9b618927024cf8099317733f8abac043114f72
Jesus Emilio Osorio Perez <jeosoriop@ufpso.edu.co>
2024-11-02 22:28:10 -0500

    login: loginRight

--- src/app/login/components/login-form/loginForm.tsx
+++ src/app/login/components/login-form/loginForm.tsx
@@ -1,5 +1,25 @@
 import styles from './styles.module.css'
 
 export function LoginForm() {
-  return <div className={styles.loginForm}>loginForm</div>
+  return (
+    <div className={styles.loginForm}>
+      <h2 className='text-3xl'>Inicia sesión</h2>
+      <form className={styles.form}>
+        <input
+          type='email'
+          name='email'
+          placeholder='Ingresa tu email'
+        />
+
+        <input
+          type='password'
+          name='password'
+          placeholder='Ingresa tu contraseña'
+        />
+        <p>¿Olvidaste tu contraseña?</p>
+
+        <button>Iniciar sesión</button>
+      </form>
+    </div>
+  )
 }
