b766ad0295170323999e8bef3bd32401a7de49bd
Jesus Emilio <jeosoriop@ufpso.edu.co>
2024-11-16 16:50:50 -0500

    collaborators: start collaborators view, with components

---
+++ src/app/application/collaborators/components/collaborators-view/collaboratorView.tsx
@@ -0,0 +1,22 @@
+import type { User } from '@/app/login/components/login-form/interfaces'
+
+interface Props {
+  user: User
+}
+
+export function CollaboratorView({ user }: Props) {
+  return (
+    <div className='flex bg-[var(--white-100)] gap-4  p-2 rounded-sm'>
+      <img
+        src={user.image}
+        alt=''
+        className='h-12 w-12 rounded-full object-cover'
+      />
+
+      <div className='flex flex-col gap-1'>
+        <p>{user.name}</p>
+        <p className='text-[--jordy-blue-400]'>{user.role}</p>
+      </div>
+    </div>
+  )
+}
--- src/app/application/collaborators/page.tsx
+++ src/app/application/collaborators/page.tsx
@@ -1,10 +1,11 @@
-import { CollaboratorView } from './components/collaboratorView'
+import { mockUsers } from '@/app/login/components/login-form/constants'
+import { CollaboratorView } from './components/collaborators-view/collaboratorView'
 
 export default function Page() {
   return (
     <section className='flex flex-col gap-2 p-2'>
       <p>Lista de colaboradores</p>
-      <CollaboratorView />
+      <CollaboratorView user={mockUsers[0]}/>
     </section>
   )
 }
