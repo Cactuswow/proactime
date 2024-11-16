815cba6c5b86319b3ddc279f66ab077f268bc2fd
Jesus Emilio <jeosoriop@ufpso.edu.co>
2024-11-16 15:36:39 -0500

    collaborators: start collaborators view, edit better

--- src/app/application/collaborators/components/collaboratorView.tsx
+++ src/app/application/collaborators/components/collaboratorView.tsx
@@ -1,15 +1,15 @@
 export function CollaboratorView() {
   return (
-    <div className='flex bg-slate-400 gap-2'>
+    <div className='flex bg-[var(--white-100)] gap-4  p-2 rounded-sm'>
       <img
         src='https://i.pinimg.com/236x/53/c5/8b/53c58be968dd9c29d59a7d430537bad1.jpg'
         alt=''
-        className='h-11 w-11'
+        className='h-12 w-12 rounded-full object-cover'
       />
 
-      <div>
+      <div className='flex flex-col gap-1'>
         <p>Colaborador 1</p>
-        <p className='text-red-600'>rol</p>
+        <p className='text-[--jordy-blue-400]'>rol</p>
       </div>
     </div>
   )
