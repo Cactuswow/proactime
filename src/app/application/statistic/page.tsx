5c2d868392854b8e847a9e6c7c1d5e9ac48bd932
Kactuswow <bryanalvarezg.kcts@gmail.com>
2024-11-16 15:36:47 -0500

    statistic: calendar and task list

--- src/app/application/statistic/page.tsx
+++ src/app/application/statistic/page.tsx
@@ -1,12 +1,20 @@
+import { mockTasks } from '../constants'
+import { TaskList } from '../critical-path/components/task-list/taskList'
 import { Calendar } from './components/calendar/calendar'
+import { months } from './components/calendar/constants'
 
 export default function Page() {
+  const date = new Date()
+
   return (
     <main className='flex flex-1'>
       <div className='flex flex-1'>a</div>
-      <div className='flex flex-col gap-3 flex-1 p-2 max-w-md border-l-2'>
-        <p>Calendario</p>
+      <div className='flex flex-col gap-3 flex-1 p-2 max-w-sm border-l-2'>
+        <p>
+          Calendario ({months[date.getMonth()]} {date.getFullYear()})
+        </p>
         <Calendar />
+        <TaskList tasks={mockTasks} />
       </div>
     </main>
   )
