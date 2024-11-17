eabb207bcc468c8b2947d4ad03cb58a1fa773715
Jesus Emilio <jeosoriop@ufpso.edu.co>
2024-11-16 22:40:25 -0500

    collaborators: collaboratorsDelete view

---
+++ src/app/application/collaborators/components/collaborators-delete/collaboratorsDelete.tsx
@@ -0,0 +1,18 @@
+import { mockUsers } from '@/app/login/components/login-form/constants'
+import { CollaboratorView } from '../collaborators-view/collaboratorView'
+
+export function CollaboratorsDelete() {
+  return (
+    <div className='flex'>
+      <div className='flex-1'>
+        <CollaboratorView user={mockUsers[0]} />
+      </div>
+      <button
+        type='submit'
+        className='bg-[var(--sweet-pink-300)] w-40 rounded-sm p-2'
+      >
+        Eliminar
+      </button>
+    </div>
+  )
+}
--- src/app/application/collaborators/page.tsx
+++ src/app/application/collaborators/page.tsx
@@ -1,14 +1,18 @@
 import { mockUsers } from '@/app/login/components/login-form/constants'
 import { CollaboratorView } from './components/collaborators-view/collaboratorView'
 import { CollaboratorSearch } from './components/collaborators-search/collaboratorSearch'
+import { CollaboratorsDelete } from './components/collaborators-delete/collaboratorsDelete'
 
 export default function Page() {
   return (
     <div className='flex justify-center'>
       <section className='flex flex-col gap-2 p-2  max-w-7xl w-full'>
+
       <CollaboratorSearch/>
+
         <p>Lista de colaboradores</p>
         <CollaboratorView user={mockUsers[0]} />
+        <CollaboratorsDelete/>
       </section>
     </div>
   )
