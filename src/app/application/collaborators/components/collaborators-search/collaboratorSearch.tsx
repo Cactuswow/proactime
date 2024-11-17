7dde4da7834e6ffe085b014aafe204c20f665bc6
Jesus Emilio <jeosoriop@ufpso.edu.co>
2024-11-16 22:14:32 -0500

    collaborators: collaborators search

---
+++ src/app/application/collaborators/components/collaborators-search/collaboratorSearch.tsx
@@ -0,0 +1,9 @@
+
+export function CollaboratorSearch() {
+    return (
+      <form action="" className="flex justify-between">
+        <input type="text" placeholder="Buscar Colaboradores" className="w-1/2 border rounded-sm pl-2 outline-none p-3"/>
+        <button type="submit" className="p-3 px-3 bg-[--jordy-blue-200] rounded-md">Agregar Colaborador</button>
+      </form>
+    )
+  }
